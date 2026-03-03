d3eeb056941fcf45aaa8d4548d14b57d48c54f54
Ingo Wolf <me@ingowolf.au>
2026-03-03 15:17:02 +1100

    update covnex client provider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { PUBLIC_CONVEX_URL } from '$env/static/public';
-	import { setupConvex } from 'convex-svelte';
+	import { createSvelteAuthClient } from '@mmailaender/convex-better-auth-svelte/svelte';
+
+	import { authClient } from '$lib/auth-client';
+	createSvelteAuthClient({ authClient });
 
 	const { children } = $props();
-	setupConvex(PUBLIC_CONVEX_URL);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
