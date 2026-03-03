3cbbc0fbaafad1b50e41c7a0be794ba8896b1a80
Ingo Wolf <me@ingowolf.au>
2026-03-03 15:29:31 +1100

    fix lint error+format

--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -1,10 +1,10 @@
-import { createClient, type GenericCtx } from "@convex-dev/better-auth";
-import { convex } from "@convex-dev/better-auth/plugins";
-import { components } from "./_generated/api";
-import { type DataModel } from "./_generated/dataModel";
-import { query } from "./_generated/server";
-import { betterAuth, type BetterAuthOptions } from "better-auth/minimal";
-import authConfig from "./auth.config";
+import { createClient, type GenericCtx } from '@convex-dev/better-auth';
+import { convex } from '@convex-dev/better-auth/plugins';
+import { components } from './_generated/api';
+import { type DataModel } from './_generated/dataModel';
+import { query } from './_generated/server';
+import { betterAuth } from 'better-auth/minimal';
+import authConfig from './auth.config';
 
 const siteUrl = process.env.SITE_URL!;
 
@@ -13,26 +13,26 @@ const siteUrl = process.env.SITE_URL!;
 export const authComponent = createClient<DataModel>(components.betterAuth);
 
 export const createAuth = (ctx: GenericCtx<DataModel>) => {
-  return betterAuth({
-    baseURL: siteUrl,
-    database: authComponent.adapter(ctx),
-    // Configure simple, non-verified email/password to get started
-    emailAndPassword: {
-      enabled: true,
-      requireEmailVerification: false,
-    },
-    plugins: [
-      // The Convex plugin is required for Convex compatibility
-      convex({ authConfig }),
-    ],
-  })
-}
+	return betterAuth({
+		baseURL: siteUrl,
+		database: authComponent.adapter(ctx),
+		// Configure simple, non-verified email/password to get started
+		emailAndPassword: {
+			enabled: true,
+			requireEmailVerification: false
+		},
+		plugins: [
+			// The Convex plugin is required for Convex compatibility
+			convex({ authConfig })
+		]
+	});
+};
 
 // Example function for getting the current user
 // Feel free to edit, omit, etc.
 export const getCurrentUser = query({
-  args: {},
-  handler: async (ctx) => {
-    return authComponent.getAuthUser(ctx);
-  },
+	args: {},
+	handler: async (ctx) => {
+		return authComponent.getAuthUser(ctx);
+	}
 });
