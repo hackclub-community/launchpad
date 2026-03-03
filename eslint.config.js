d267a793310997afe8ab808c3eca700982b1d91c
Ingo Wolf <me@ingowolf.au>
2026-03-03 17:47:24 +1100

    you will use typescript

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,14 @@ export default defineConfig(
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			'no-undef': 'off',
+			'svelte/block-lang': [
+				'error',
+				{
+					// I will force typescript onto you and you will not complain
+					script: 'ts'
+				}
+			]
 		}
 	},
 	{
