3cf2ba85ff4185be959027ed30ba08eb92e76ecb
Ingo Wolf <me@ingowolf.au>
2026-03-03 15:17:38 +1100

    add hook

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,9 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+			token: string | undefined;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
