c3601fca85e53ba104880e2c60657a7cce17ecb8
Ingo Wolf <me@ingowolf.au>
2026-03-03 14:44:08 +1100

    add convex setup stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { setupConvex } from 'convex-svelte';
 
-	let { children } = $props();
+	const { children } = $props();
+	setupConvex(PUBLIC_CONVEX_URL);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
