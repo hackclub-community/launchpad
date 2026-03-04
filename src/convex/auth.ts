15c263f488595f7da987feaecdf04daf065a4abd
Ingo Wolf <me@ingowolf.au>
2026-03-04 22:56:10 +1100

    add scopes

--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -33,7 +33,8 @@ export const createAuthOptions = (ctx: GenericCtx<DataModel>) => {
 						providerId: 'hca',
 						clientId: process.env.HCA_CLIENT_ID!,
 						clientSecret: process.env.HCA_CLIENT_SECRET!,
-						discoveryUrl: 'https://auth.hackclub.com/.well-known/openid-configuration'
+						discoveryUrl: 'https://auth.hackclub.com/.well-known/openid-configuration',
+						scopes: ['openid', 'email', 'name', 'profile']
 					}
 				]
 			})
