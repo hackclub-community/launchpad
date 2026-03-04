e45b900e51e5714bd9f50cb9b97a83064f12ec1f
Ingo Wolf <me@ingowolf.au>
2026-03-05 00:28:29 +1100

    add new sidebartoggle with cool transition

---
+++ src/lib/components/organizer/sidebar-toggle.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	import { cubicOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
+
+	const sidebar = useSidebar();
+</script>
+
+<div class="flex flex-row items-center rounded-lg border bg-sidebar p-1">
+	<Sidebar.Trigger />
+	{#if !sidebar.open}
+		<div
+			transition:slide={{ axis: 'x', duration: 200, easing: cubicOut }}
+			class="rounded-full px-1"
+		>
+			<Badge>Launchpad</Badge>
+		</div>
+	{/if}
+</div>
--- src/routes/(organizer)/+layout.svelte
+++ src/routes/(organizer)/+layout.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/organizer/app-sidebar.svelte';
-	import Badge from '$lib/components/ui/badge/badge.svelte';
-
+	import SidebarToggle from '$lib/components/organizer/sidebar-toggle.svelte';
 	let { children } = $props();
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar />
 	<main class="pt-2 pl-2">
-		<div class="flex flex-row items-center gap-1 rounded-lg border bg-sidebar p-1 pr-2">
-			<Sidebar.Trigger />
-			<Badge variant="outline">Launchpad</Badge>
-		</div>
+		<SidebarToggle />
 		{@render children?.()}
 	</main>
 </Sidebar.Provider>
