656952b28ace05564aaa6de83c7ebd3259552c50
Ingo Wolf <me@ingowolf.au>
2026-03-05 00:43:26 +1100

    update sidebar toggle

--- src/lib/components/organizer/sidebar-toggle.svelte
+++ src/lib/components/organizer/sidebar-toggle.svelte
@@ -13,9 +13,10 @@
 	{#if !sidebar.open}
 		<div
 			transition:slide={{ axis: 'x', duration: 200, easing: cubicOut }}
-			class="flex rounded-full px-1"
+			class="flex gap-1 rounded-full px-1"
 		>
-			<Badge>Launchpad</Badge>
+			<Badge>Event name</Badge>
+			<Badge variant="outline">Launchpad</Badge>
 		</div>
 	{/if}
 </div>
