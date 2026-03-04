647390ca986feb13a491690692420a74b6a2970f
Ingo Wolf <me@ingowolf.au>
2026-03-04 23:43:30 +1100

    add login compoentnt to root route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
-<div
-	class="flex min-h-screen w-full flex-col items-center justify-center bg-gradient-to-br from-primary to-secondary px-4 text-center text-white"
->
-	<h1 class="mb-6 text-6xl font-bold drop-shadow-lg">Welcome to Launchpad</h1>
-	<p class="mb-8 text-2xl">A new hackathon attendee portal coming soon..</p>
+<script lang="ts">
+	import Login from '$lib/components/login.svelte';
+</script>
 
-	<a
-		href="/login"
-		class="rounded-lg bg-white px-8 py-4 font-semibold text-primary shadow-lg transition hover:bg-gray-100"
-	>
-		Login
-	</a>
-</div>
+<Login />
