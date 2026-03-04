d00d613e8d8ee151badee72984f37017faf73ece
Ingo Wolf <me@ingowolf.au>
2026-03-05 00:39:56 +1100

    add title

--- src/lib/components/organizer/app-sidebar.svelte
+++ src/lib/components/organizer/app-sidebar.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <Sidebar.Root variant="floating">
-	<Sidebar.Header />
+	<Sidebar.Header>
+		<Sidebar.Menu>
+			<Sidebar.MenuItem class="flex h-9 items-center rounded-full bg-card pl-12">
+				Launchpad
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Header>
+
 	<Sidebar.Content>
 		<Sidebar.Group />
 		<Sidebar.Group />
