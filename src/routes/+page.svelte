c3f24f04554ad31376d09951a7433f01c7e0e6bc
Ingo Wolf <me@ingowolf.au>
2026-03-05 09:35:03 +1100

    add login page background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import Login from '$lib/components/login.svelte';
+	import crowdImage from '$lib/assets/crowd.jpeg?enhanced';
 </script>
 
-<div class="flex h-screen items-center justify-start px-10">
+<div
+	class="relative z-10 flex h-screen w-full items-center justify-start bg-linear-to-r from-background/75 to-transparent px-10"
+>
 	<Login />
 </div>
+<enhanced:img src={crowdImage} class="fixed top-0 left-0 h-full w-full object-cover" />
