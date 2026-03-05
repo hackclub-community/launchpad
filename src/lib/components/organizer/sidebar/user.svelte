a774ceacff4277eeea7b86f8fa86da72188622ee
Ingo Wolf <me@ingowolf.au>
2026-03-05 18:44:43 +1100

    move user profile to component

---
+++ src/lib/components/organizer/sidebar/user.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import { ChevronUpIcon } from '@lucide/svelte';
+</script>
+
+<Sidebar.MenuItem>
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger class="w-full">
+			<Sidebar.MenuButton size="lg" class="cursor-pointer border shadow-xs">
+				<Avatar.Root>
+					<Avatar.Fallback>U</Avatar.Fallback>
+				</Avatar.Root>
+				Username
+				<ChevronUpIcon class="mr-1 ml-auto" />
+			</Sidebar.MenuButton>
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content align="end">
+			<DropdownMenu.Group>
+				<DropdownMenu.Label>My Account</DropdownMenu.Label>
+				<DropdownMenu.Separator />
+				<DropdownMenu.Item>Settings</DropdownMenu.Item>
+				<DropdownMenu.Item>Sign out</DropdownMenu.Item>
+			</DropdownMenu.Group>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
+</Sidebar.MenuItem>
