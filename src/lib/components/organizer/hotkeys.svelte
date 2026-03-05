e2dbb9d73d4b5452b6f7728c67dccdf8b1616484
Ingo Wolf <me@ingowolf.au>
2026-03-05 14:53:19 +1100

    add hotkeys

---
+++ src/lib/components/organizer/hotkeys.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import hotkeys from 'hotkeys-js';
+	import { onMount } from 'svelte';
+	import { searchState } from './search-state.svelte';
+
+	onMount(() => {
+		hotkeys('ctrl+k,command+k', (e) => {
+			searchState.open = true;
+			e.preventDefault();
+		});
+
+		return () => hotkeys.unbind('ctrl+s,command+s');
+	});
+</script>
--- src/routes/(organizer)/+layout.svelte
+++ src/routes/(organizer)/+layout.svelte
@@ -3,12 +3,14 @@
 	import AppSidebar from '$lib/components/organizer/app-sidebar.svelte';
 	import SidebarToggle from '$lib/components/organizer/sidebar-toggle.svelte';
 	import Search from '$lib/components/organizer/search.svelte';
+	import Hotkeys from '$lib/components/organizer/hotkeys.svelte';
 	let { children } = $props();
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar />
 	<Search />
+	<Hotkeys />
 	<main class="pt-2 pl-2">
 		<SidebarToggle />
 		{@render children?.()}
