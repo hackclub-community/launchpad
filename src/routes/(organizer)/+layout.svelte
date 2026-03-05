c5da9986e223cdda8f4482bfb65a4addf957935f
Ingo Wolf <me@ingowolf.au>
2026-03-05 13:54:35 +1100

    add search

--- src/routes/(organizer)/+layout.svelte
+++ src/routes/(organizer)/+layout.svelte
@@ -2,11 +2,13 @@
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/organizer/app-sidebar.svelte';
 	import SidebarToggle from '$lib/components/organizer/sidebar-toggle.svelte';
+	import Search from '$lib/components/organizer/search.svelte';
 	let { children } = $props();
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar />
+	<Search />
 	<main class="pt-2 pl-2">
 		<SidebarToggle />
 		{@render children?.()}
