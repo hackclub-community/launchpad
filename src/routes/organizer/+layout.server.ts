84c5d832d2c9c983aedc730129493927d5b3cf38
Ingo Wolf <me@ingowolf.au>
2026-03-07 11:07:33 +1100

    add rediect

--- src/routes/organizer/+layout.server.ts
+++ src/routes/organizer/+layout.server.ts
@@ -5,12 +5,13 @@ import {
 	createConvexHttpClient,
 	getAuthState
 } from '@mmailaender/convex-better-auth-svelte/sveltekit';
+import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals, cookies }) => {
 	const authState = await getAuthState(createAuth, cookies);
 
 	if (!authState.isAuthenticated) {
-		return { authState, currentUser: null };
+		throw redirect(302, '/');
 	}
 
 	const client = createConvexHttpClient({ token: locals.token });
