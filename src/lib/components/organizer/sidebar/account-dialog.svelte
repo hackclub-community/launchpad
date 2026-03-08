f19d76fcd1ddf52543b8019b2e68cc7cd874deb0
Ingo Wolf <me@ingowolf.au>
2026-03-08 17:20:08 +1100

    use drawer on mobile

--- src/lib/components/organizer/sidebar/account-dialog.svelte
+++ src/lib/components/organizer/sidebar/account-dialog.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import { useSidebar } from '$lib/components/ui/sidebar';
 
 	let { open = $bindable() }: { open: boolean } = $props();
+
+	const sidebar = useSidebar();
 </script>
 
-<Dialog.Root bind:open>
-	<Dialog.Content>
-		<Dialog.Header>
-			<Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
-			<Dialog.Description>
-				This action cannot be undone. This will permanently delete your account and remove your data
-				from our servers.
-			</Dialog.Description>
-		</Dialog.Header>
-	</Dialog.Content>
-</Dialog.Root>
+{#if sidebar.isMobile}
+	<Drawer.Root bind:open>
+		<Drawer.Content>
+			<Drawer.Header>
+				<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
+				<Drawer.Description>
+					This action cannot be undone. This will permanently delete your account and remove your
+					data from our servers.
+				</Drawer.Description>
+			</Drawer.Header>
+		</Drawer.Content>
+	</Drawer.Root>
+{:else}
+	<Dialog.Root bind:open>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
+				<Dialog.Description>
+					This action cannot be undone. This will permanently delete your account and remove your
+					data from our servers.
+				</Dialog.Description>
+			</Dialog.Header>
+		</Dialog.Content>
+	</Dialog.Root>
+{/if}
