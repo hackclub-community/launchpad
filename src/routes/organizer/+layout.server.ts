303fbd59a60b7450443f717ff084cdfab275261e
Ingo Wolf <me@ingo.au>
2026-05-12 14:41:50 +1000

    clean up

--- src/routes/organizer/+layout.server.ts
+++ src/routes/organizer/+layout.server.ts
@@ -1,20 +1,19 @@
 import type { LayoutServerLoad } from './$types';
 import { api } from '$convex/_generated/api';
-import { createAuth } from '$convex/auth.js';
 import {
 	createConvexHttpClient,
 	getAuthState
 } from '@mmailaender/convex-better-auth-svelte/sveltekit';
 import { redirect } from '@sveltejs/kit';
 
-export const load: LayoutServerLoad = async ({ locals, cookies }) => {
-	const authState = await getAuthState(createAuth, cookies);
+export const load: LayoutServerLoad = async () => {
+	const authState = getAuthState();
 
 	if (!authState.isAuthenticated) {
 		throw redirect(302, '/');
 	}
 
-	const client = createConvexHttpClient({ token: locals.token });
+	const client = createConvexHttpClient();
 
 	try {
 		const currentUser = await client.query(api.auth.getCurrentUser, {});
